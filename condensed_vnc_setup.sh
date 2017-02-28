1bca516ed5f25d155ef365c11f328caee1a7f446
Ben-Hu <benjqh@gmail.com>
2017-02-28 01:10:32 -0500

    add condensed + basic VNC server setup

---
+++ condensed_vnc_setup.sh
@@ -0,0 +1,50 @@
+sudo apt-get update
+sudo apt-get install xfce4 xfce4-goodies tightvncserver
+
+#Should also install xvfb for the virtual frame buffer if you are using 
+#VNC to work around Unity's requirement to have a display for initial license agreement 
+#when building on a headless server
+
+vncserver
+
+vncserver -kill :1
+
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+vim ~/.vnc/xstartup
+
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+
+sudo chmod +x ~/.vnc/xstartup
+
+vim /etc/init.d/vncserver
+
+#!/bin/bash
+PATH="$PATH:/usr/bin/"
+export USER="user"
+DISPLAY="1"
+DEPTH="16"
+GEOMETRY="1024x768"
+OPTIONS="-depth ${DEPTH} -geometry ${GEOMETRY} :${DISPLAY} -localhost"
+. /lib/lsb/init-functions
+case "$1" in
+start)
+log_action_begin_msg "Starting vncserver for user '${USER}' on localhost:${DISPLAY}"
+su ${USER} -c "/usr/bin/vncserver ${OPTIONS}"
+;;
+stop)
+log_action_begin_msg "Stopping vncserver for user '${USER}' on localhost:${DISPLAY}"
+su ${USER} -c "/usr/bin/vncserver -kill :${DISPLAY}"
+;;
+restart)
+$0 stop
+$0 start
+;;
+esac
+exit 0
+
+chmod +x /etc/init.d/vncserver
+service vncserver start
+update-rc.d vncserver defaults
+
