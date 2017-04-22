aaef075c96a6a68e21d5c29bb3d13351d6a5feb7
Ben-Hu <Benjqh@gmail.com>
2017-04-22 18:14:18 -0400

    oops old verstion

--- gdrive_notes.sh
+++ gdrive_notes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for file in $(ls); do if [ $(echo $file | grep -iE 'txt') ]; then mv $file $file.txt; fi; done
-gdrive upload --recursive $(basename pwd)
+for file in $(ls); do if [ ! $(echo $file | grep -iE 'txt') ]; then mv $file $file.txt; fi; done
+gdrive upload --recursive $(pwd)
 
